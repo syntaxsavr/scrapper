a9d933303a1fe733c785f2e488df555f7322d820
Samuel Z <sumpfsamuel@gmail.com>
2025-11-23 18:23:51 +0100

    sample env + environment dsn for sentry

--- scrapper_project/settings.py
+++ scrapper_project/settings.py
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Sentry Configuration
 sentry_sdk.init(
-    dsn="xxxxxxxxxxxxxxxxxxxxxxxxx",
+    dsn=os.getenv("SENTRY_DSN", ""),
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
