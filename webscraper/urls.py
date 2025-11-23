8834bbaa70f31c5b0635d88750389d9a2a7b2edb
deorhan <deorhan@edu.aau.at>
2025-11-23 14:22:33 +0100

    User Login/Signup with Inbuilt Functions, Persistent Login

--- webscraper/urls.py
+++ webscraper/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
-urlpatterns = [path("", views.home_view, name="home")]
+urlpatterns = [
+    path("", views.home_view, name="home"),
+    path("login/", views.login_view, name="login"),
+    path("signup/", views.signup_view, name="signup"),
+    path("logout/", LogoutView.as_view(next_page="home"), name="logout")
+    ]
--- webscraper/views.py
+++ webscraper/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .tasks import background_work
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from .models import Log
 
 def home_view(request):
@@ -8,6 +10,34 @@ def home_view(request):
         "search": search,
     }
     return render(request, "home.html", context)
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+    
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+def signup_view(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    return render(request, "signup.html", {"form": form})
 
 def index(request):
     if request.method == 'POST':
