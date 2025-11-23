6f08c6a06b324ea1d5993dda7383f686543d4889
Samuel Z <sumpfsamuel@gmail.com>
2025-11-23 18:52:33 +0100

    actions, deploy to cockerhub

--- scrapper_project/settings.py
+++ scrapper_project/settings.py
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Sentry Configuration
 sentry_sdk.init(
-    dsn=os.getenv("SENTRY_DSN", ""),
+    dsn=os.getenv("SENTRY_DSN", ""), # Sentry DSN from environment variable, dont use if emppty to not get interrupted at dev
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
