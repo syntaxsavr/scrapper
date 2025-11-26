a4b85b0724b043d3c9ebc773d5fa742e4d3b41eb
mattku <matthiasku@edu.aau.at>
2025-11-26 22:43:58 +0100

    Revert "mult_numbers test case"
    This reverts commit 3b3e554b2b6d8f97c3df926ffe71874eca4caa29.

--- webscraper/testExample.py
+++ webscraper/testExample.py
@@ -6,5 +6,6 @@ def add_numbers(a, b):
 def sub_numbers(a, b):
     return a - b
 
-def mult_numbers(a, b):
-    return a * b
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
