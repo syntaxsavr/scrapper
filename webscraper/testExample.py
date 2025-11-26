5c4979061c4cb91f502744edd84cac89af619fba
mattku <matthiasku@edu.aau.at>
2025-11-26 21:59:06 +0100

    mult_numbers test case

--- webscraper/testExample.py
+++ webscraper/testExample.py
@@ -6,6 +6,5 @@ def add_numbers(a, b):
 def sub_numbers(a, b):
     return a - b
 
-def test_add_numbers():
-    assert add_numbers(2, 3) == 5
-    assert add_numbers(-1, 1) == 0
+def mult_numbers(a, b):
+    return a * b
