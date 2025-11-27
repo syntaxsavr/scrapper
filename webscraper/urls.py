f159ec65753316d6110faa4aee5c57140c3c119e
david-writes-code <david.schmid.git@gmail.com>
2025-11-27 14:41:06 +0100

    added URL route

--- webscraper/urls.py
+++ webscraper/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
 
     path("api/search/", views.api_search, name="api_search"),
     path("api/status/<str:task_id>/", views.api_task_status, name="api_task_status"),
+    path("api/scrape-hugging-face-search/", views.api_scrape_hugging_face_search,
+         name="api_scrape_hugging_face_search")
 ]
