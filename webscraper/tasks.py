e7cb19708902654a67db98221f187f3a8ea57e79
david-writes-code <david.schmid.git@gmail.com>
2025-11-27 14:37:14 +0100

    added celery task similar to dataset search

--- webscraper/tasks.py
+++ webscraper/tasks.py
@@ -1,4 +1,7 @@
 from celery import shared_task
+from setuptools.compat.py311 import shutil_rmtree
+
+from scrapers.hugging_face.scraper_hugging_face import scrape_hugging_face_datasets
 from .models import Log, Dataset
 import time
 
@@ -15,4 +18,20 @@ def search_datasets(query):
         "query": query,
         "count": count,
         "results": list(results.values("id", "title", "description"))
+    }
+
+@shared_task
+def run_hugging_face_search_task(query: str, limit: int = 50):
+    scraped_items = scrape_hugging_face_datasets(query=query, limit=limit)
+    added_count = 0
+
+    for item in scraped_items:
+        if not Dataset.objects.filter(title=item["title"]).exists():
+            Dataset.objects.create(title=item["title"], description=item["description"])
+            added_count += 1
+
+    return {
+        "query": query,
+        "total_scraped": len(scraped_items),
+        "added": added_count
     }
