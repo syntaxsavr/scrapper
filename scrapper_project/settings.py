23518c4d7a2c73382243841230dcdeff77d6c665
mattku <matthiasku@edu.aau.at>
2025-11-29 17:30:25 +0100

    Revert "Merge branch 'feature/basic-scraper' of https://github.com/syntaxsavr/scrapper into feature/basic-scraper"
    This reverts commit 816001e2f842183c94bb319494cd3b5c55e77274, reversing
    changes made to 03e0244e8c168e396282e9be5c01f97b195898f2.

--- scrapper_project/settings.py
+++ scrapper_project/settings.py
@@ -42,7 +42,7 @@ ROOT_URLCONF = 'scrapper_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -87,8 +87,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # SECURITY (Production only)
 if ENVIRONMENT == 'production':
