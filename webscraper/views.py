441d429946f1d0f1be7086274e6651ecfb43b24e
mattku <matthiasku@edu.aau.at>
2025-11-29 19:10:35 +0100

    Reapply "Merge branch 'feature/basic-scraper' of https://github.com/syntaxsavr/scrapper into feature/basic-scraper"
    This reverts commit c4b86308e6b748144110463c616aae00358866c8.

--- webscraper/views.py
+++ webscraper/views.py
@@ -1,13 +1,99 @@
-from django.shortcuts import render
-from scrapper_project.settings import ENVIRONMENT
-from .tasks import background_work
-from .models import Log
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
 
-def index(request):
-    if request.method == 'POST':
-        background_work.delay()
-    logs = Log.objects.all().order_by('-created_at')[:5]
-    return render(request, 'webscraper/index.html', {
-        'logs': logs, 
-        'environment': ENVIRONMENT
+from .tasks import search_datasets, run_hugging_face_search_task
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
+from .models import Log, Dataset
+from celery.result import AsyncResult
+
+def home_view(request):
+    return render(request, "home.html")
+
+def api_search(request):
+    query = request.GET.get("q", "")
+
+    if not query:
+        return JsonResponse({"error": "No query provided"}, status=400)
+
+    task = search_datasets.delay(query)
+
+    return JsonResponse({
+        "task_id": task.id,
+        "status": "started",
+        "message": f"Search started for '{query}'",
+    })
+
+def api_task_status(_request, task_id):
+    task = AsyncResult(task_id)
+
+    if task.ready():
+        result = task.result
+
+        results = Dataset.objects.filter(
+            id__in=[r["id"] for r in result["results"]]
+        )
+
+        return JsonResponse({
+            "status": "completed",
+            "count": result["count"],
+            "results": list(results.values("id", "title", "description")),
+        })
+    else:
+        return JsonResponse({
+            "status": "pending",
+        })
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+    
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+        
+    context = {
+        "form": form
+    }
+    return render(request, "login.html", context)
+
+def signup_view(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, "signup.html", context)
+
+@require_GET
+def api_scrape_hugging_face_search(request):
+    query = request.GET.get("q")
+    if not query:
+        return JsonResponse({"error": "Query parameter 'q' is required."}, status=400)
+
+    limit = int(request.GET.get("limit", 50))
+
+    task = run_hugging_face_search_task.delay(query, limit)
+
+    return JsonResponse({
+        "message": f"Scraping Hugging Face datasets for '{query}' started",
+        "task_id": task.id,
+        "status": "started",
+        "limit": limit
     })
