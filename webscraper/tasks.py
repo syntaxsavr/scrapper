73d8868e92f88ed244864486886c546ff16c1979
mattku <matthiasku@edu.aau.at>
2025-11-29 18:07:07 +0100

    Reapply "Merge branch 'ci-pipleline' of https://github.com/syntaxsavr/scrapper into ci-pipleline"
    This reverts commit 6b66610b303f8a37be4bfca87826a53c9854c282.

--- webscraper/tasks.py
+++ webscraper/tasks.py
@@ -1,9 +1,18 @@
 from celery import shared_task
-from .models import Log
+from .models import Log, Dataset
 import time
 
 @shared_task
-def background_work():
-    time.sleep(2)
-    Log.objects.create(message=f"Task completed in {ENVIRONMENT}!")
-    return "Done"
+def search_datasets(query):
+    from django.db.models import Q
+    results = Dataset.objects.filter(
+        Q(title__icontains=query) | Q(description__icontains=query)
+    )
+
+    count = results.count()
+
+    return {
+        "query": query,
+        "count": count,
+        "results": list(results.values("id", "title", "description"))
+    }
