0606b88263bc2aa170360d464349328807737114
mattku <matthiasku@edu.aau.at>
2026-01-05 17:59:26 +0100

    Initial Kaggle Scraper
    The scraper under scraper_kaggle.py uses selenium to load the page and navigate through it, beautiful soup to parse it.

--- scrapers/kaggle/kaggle_scraper.py
+++ scrapers/kaggle/kaggle_scraper.py
@@ -1,12 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import quote_plus
+
+# beautiful soup and plain requests are not enough - the kaggle delivers a barebone site, everything is built by js later on.
+
 
 class KaggleScraper:
-    def __init__(self, base_url="https://www.kaggle.com/datasets"):
+    def __init__(self, base_url="https://www.kaggle.com"):
         self.base_url = base_url
 
-    def fetch_page(self):
-        response = requests.get(self.base_url)
+    def build_search_url(self, term: str) -> str:
+        if not term.strip():
+            return f"{self.base_url}/datasets"
+
+        encoded = quote_plus(term.strip())
+        return f"{self.base_url}/search?q={encoded}+date%3A1"  # limit to results from last day
+
+    def fetch_page(self, term: str):
+        # response = requests.get(self.base_url)
+        # response.raise_for_status()
+        # return response.text
+        url = self.build_search_url(term)
+        # try to fake being a browser
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en,de;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+        }
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.text
 
@@ -15,7 +38,24 @@ class KaggleScraper:
         titles = [a.get_text(strip=True) for a in soup.select("a.sc-1x5rzg1-1")]
         return titles
 
-    def scrape(self):
-        html = self.fetch_page()
+    def scrape(self, term: str):
+        html = self.fetch_page(term)
         datasets = self.parse_datasets(html)
         return datasets
+
+
+if __name__ == "__main__":
+    term = "houses"
+    scraper = KaggleScraper()
+    html = scraper.fetch_page(term)
+
+    print("\n RAW HTML")
+    print(html)
+
+    print("\n PARSED TITLES")
+    datasets = scraper.parse_datasets(html)
+
+    for title in datasets:
+        print("-", title)
+
+    print(f"\nTotal parsed datasets: {len(datasets)}")
