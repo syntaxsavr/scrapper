19be5a875b5faaee71cafd27d81d053bedf05fd0
mattku <matthiasku@edu.aau.at>
2026-01-06 17:11:19 +0100

    Added limit to scraping

--- scrapers/kaggle/scraper_kaggle_service.py
+++ scrapers/kaggle/scraper_kaggle_service.py
@@ -4,10 +4,9 @@ from scraper_kaggle import KaggleScraperSelenium
 from contextlib import asynccontextmanager
 from threading import Lock
 
+
 # how to run this?
 # uvicorn scraper_kaggle_service:app --host 127.0.0.1 --port 8101
-
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global scraper
@@ -30,14 +29,14 @@ busy_lock = Lock()
 
 
 @app.post("/scrape")
-async def scrape(query: str):
+async def scrape(query: str, limit: int):
     acquired = busy_lock.acquire(blocking=False)
     if not acquired:
-        raise HTTPException(status_code=423, status="busy")
+        raise HTTPException(status_code=423, detail="busy")
 
     try:
         # results = scraper.scrape(query)
-        results = await run_in_threadpool(scraper.scrape, query)
+        results = await run_in_threadpool(scraper.scrape, query, limit)
         ans = []
         for r in results:
             ans.append(
